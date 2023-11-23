fde8e8995c582e82d356bbfc628a489d283651a7
Square <2499532412@qq.com>
2023-11-23 15:49:56 +0800

    modify plot diagram method

--- util/pilot.py
+++ util/pilot.py
@@ -5,23 +5,27 @@ from util.request_exchange_rate import RequestExchangeRate
 
 def plot_matrix(matrix):
     plt.matshow(matrix)
-    # remove the background color
 
-    # set the line number of the matrix
-    # plt.xticks(RequestExchangeRate.currencies)
-    # plt.yticks(RequestExchangeRate.currencies)
-    plt.imshow(matrix, cmap=plt.cm.Blues)
+    # set the col and row label of the matrix
+    col_labels = row_labels = RequestExchangeRate.currencies
+    plt.xticks(range(len(matrix)), col_labels)
+    plt.yticks(range(len(matrix)), row_labels)
+
+    # set the color of the text
+    # plt.imshow(matrix, cmap=plt.cm.Blues)
+    import matplotlib.colors
+    my_cmap = matplotlib.colors.ListedColormap(['white'])
+    plt.imshow(matrix, cmap=my_cmap)
+
     for i in range(len(matrix)):
         for j in range(len(matrix)):
             # format the number
             matrix[i][j] = "{:.4f}".format(matrix[i][j])
             plt.text(i, j, matrix[i][j], va='center', ha='center')
-    # set resolution as 1080p
+
+    # set resolutions
     fig = plt.gcf()
-    fig.set_size_inches(19.20, 10.80)
+    fig.set_size_inches(10, 10)
     plt.savefig("matrix.png", dpi=100)
+
     plt.show()
-
-
-if __name__ == '__main__':
-    plot_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
